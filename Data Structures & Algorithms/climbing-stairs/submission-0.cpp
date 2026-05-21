97b67eb58d245129c32987d6cfc00ff8ab87a21d
Bhanderi Bansil <bansilbhanderi08@gmail.com>
2026-05-21 20:11:27 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>ans(n+1);
+        if(n==1)return 1;
+        if(n==2)return 2;
+        ans[0]=0;
+        ans[1]=1;
+        ans[2]=2;
+        for(int i=3;i<=n;i++)ans[i]=ans[i-1]+ans[i-2];
+        return ans[n];
+    }
+};
